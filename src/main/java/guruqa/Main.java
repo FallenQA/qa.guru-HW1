5a774f3a2ec34820046a868debd1f348bd6a0cb7
Stanislav Kireev <118201544+FallenQA@users.noreply.github.com>
2023-02-15 21:41:45 +0300

    Update Main.java

--- src/main/java/guruqa/Main.java
+++ src/main/java/guruqa/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
     int a = 18;
     int b = 2;
-            System.out.println("Hello Collegue! Well Done!");
+            System.out.println("Hello Collegue! Well Done!!!!!");
             System.out.println(a*b);
     }
 }
